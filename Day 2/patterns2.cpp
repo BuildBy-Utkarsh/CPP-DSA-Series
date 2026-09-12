4df83e1bbd825726f7ea360bd3775b6c473e27b6
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-09-12 11:27:13 +0530

    changes upgrade

--- Day 2/patterns2.cpp
+++ Day 2/patterns2.cpp
@@ -53,20 +53,90 @@ int main(){
 
     //pattern 4
 
-    int n;
-    cout << "provide input : ";
-    cin>>n;
-    int row =1;
-    int count=1;
-    while(row<=n){
-        int col =1;
-        while(col<=row){
-            cout << count <<" ";
-            col++;
-            count++;
-        }
-        cout<<endl;
-        row++;
-    }
+    // int n;
+    // cout << "provide input : ";
+    // cin>>n;
+    // int row =1;
+    // int count=1;
+    // while(row<=n){
+    //     int col =1;
+    //     while(col<=row){
+    //         cout << count <<" ";
+    //         col++;
+    //         count++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
 
+    //pattern5
+
+    //   int n;
+    // cout << "provide input : ";
+    // cin>>n;
+    // int row =1;
+   
+    // while(row<=n){
+    //     int col =1;
+    //      int value = row;
+    //     while(col<=row){
+    //         cout << value <<" ";
+    //         col++;
+    //         value++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+    //pattern 6 
+
+    // int n; 
+    // cin>>n;
+
+    // int  i = 1;
+    // while(i<=n){
+    //     int j = 1;
+    //     while(j<=i){
+    //         cout << i-j+1 << " ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    // }
+
+    // pattern 7
+
+    // int n;
+    // cin >>n;
+    // int row =1;
+    // while(row <=n){
+    //     int col =1;
+    //     char ch = 'A'+ row -1;
+    //     while(col<=n){
+    //         cout << ch <<" ";
+    //         col++;
+    //     }
+    //     cout <<endl;
+    //     row++;
+    // }
+
+    //pattern 8
+
+      int n;
+     cin >>n;
+     int row =1;
+     while(row <=n){
+         int col =1;
+        
+            while(col<=3){
+            char ch = 'A'+ col -1;
+            cout << ch <<" ";
+            col++;
+        }
+         cout <<endl;
+         row++;
+     }
+
+
+    
 }
