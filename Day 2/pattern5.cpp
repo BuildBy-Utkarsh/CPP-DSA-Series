2d4ebe59936548cfba53439d563e403842c05e49
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-09-12 17:11:40 +0530

    added pattern 5

---
+++ Day 2/pattern5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int row = 1;
+
+    while (row <= n) {
+
+        // spaces
+        int space = row - 1;
+        while (space > 0) {
+            cout << " ";
+            space--;
+        }
+
+        // stars
+        int col = 1;
+        while (col <= n - row + 1) {
+            cout << row;
+            col++;
+        }
+
+        cout << endl;
+        row++;
+    }
+
+    return 0;
+}
