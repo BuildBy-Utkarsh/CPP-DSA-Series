3869f36aae117421efed5189affb557ec43e0974
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-09-14 17:34:04 +0530

    bitwise and for loop added

---
+++ Day 3/forLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+
+    int a = 0;
+    int b = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cout << a << " ";
+
+        int nextNumber = a + b;
+        a = b;
+        b = nextNumber;
+    }
+
+ return 0;
+}
