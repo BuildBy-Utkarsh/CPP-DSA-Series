2747a70c61a7a0660d3005f46b2eb749c6b09ade
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-09-16 15:26:40 +0530

    day 5

---
+++ Day 5/calculator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout << "enter the value of a : ";
+    cin >> a;
+    cout << "enter the value of b : ";
+    cin >> b ;
+    char op;
+    cout << "enter the operations you want to perform"  << endl;
+    cin >> op;
+    switch (op)
+    {
+    case '+': cout << (a+b) << endl;
+        break;
+    case '-': cout << (a-b) << endl;
+        break;
+    case '*': cout << (a*b) << endl;
+        break;
+    case '%': cout << (a%b) << endl;
+        break;
+    case '/': cout << (a/b) << endl;
+        break;
+    
+    default: cout << "default value";
+    }
+}
