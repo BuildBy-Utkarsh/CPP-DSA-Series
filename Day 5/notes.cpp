b6fd7248de4f244d76648d91d77444690be69dd5
Utkarsh Pratap Singh <singhutkarsh297@gmail.com>
2026-09-18 11:44:50 +0530

    changes added

--- Day 5/notes.cpp
+++ Day 5/notes.cpp
@@ -1,25 +1,69 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+//     int rupees;
+
+//     cout << "How many rupees you have: ";
+//     cin >> rupees;
+
+//     cout << "100 rupee notes: " << rupees / 100 << endl;
+//     rupees = rupees % 100;
+
+//     cout << "50 rupee notes: " << rupees / 50 << endl;
+//     rupees = rupees % 50;
+
+//     cout << "20 rupee notes: " << rupees / 20 << endl;
+//     rupees = rupees % 20;
+
+//     cout << "10 rupee notes: " << rupees / 10 << endl;
+//     rupees = rupees % 10;
+
+//     cout << "1 rupee coins: " << rupees << endl;
+
+//     return 0;
+// }
+
+
+// switch 
+
+#include <iostream>
+using namespace std;
+
 int main() {
     int rupees;
+    int choice;
 
-    cout << "How many rupees you have: ";
+    cout << "Enter rupees: ";
     cin >> rupees;
 
-    cout << "100 rupee notes: " << rupees / 100 << endl;
-    rupees = rupees % 100;
+    for (int i = 0; i < 5; i++) {
 
-    cout << "50 rupee notes: " << rupees / 50 << endl;
-    rupees = rupees % 50;
+        switch (i) {
+            case 0:
+                choice = 100;
+                break;
 
-    cout << "20 rupee notes: " << rupees / 20 << endl;
-    rupees = rupees % 20;
+            case 1:
+                choice = 50;
+                break;
 
-    cout << "10 rupee notes: " << rupees / 10 << endl;
-    rupees = rupees % 10;
+            case 2:
+                choice = 20;
+                break;
 
-    cout << "1 rupee coins: " << rupees << endl;
+            case 3:
+                choice = 10;
+                break;
+
+            case 4:
+                choice = 1;
+                break;
+        }
+
+        cout << choice << " rupee: " << rupees / choice << endl;
+        rupees = rupees % choice;
+    }
 
     return 0;
 }
